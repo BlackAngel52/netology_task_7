37edd2906f2cd41dd36318e8e82482c7af700bad
BlackAngel52 <151714758+BlackAngel52@users.noreply.github.com>
2025-07-23 20:22:22 +0300

    Add files via upload

---
+++ netology_task_7_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int q{};
+    short w{};
+    long e{};
+    long long r{};
+    float y{};
+    double u{};
+    long double i{};
+    bool o{};
+
+    std::cout << &q << " " << sizeof(q) << std::endl;
+    std::cout << &w << " " << sizeof(w) << std::endl;
+    std::cout << &e << " " << sizeof(e) << std::endl;
+    std::cout << &r << " " << sizeof(r) << std::endl;
+    std::cout << &y << " " << sizeof(y) << std::endl;
+    std::cout << &u << " " << sizeof(u) << std::endl;
+    std::cout << &i << " " << sizeof(i) << std::endl;
+    std::cout << &o << " " << sizeof(o) << std::endl;
+}
